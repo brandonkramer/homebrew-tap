b3e2a0e09a7eda77c582560ea843b327243bb83b
brandonkramer <4xsquid@gmail.com>
2026-07-19 02:45:28 +0300

    Bump netcup to 0.1.1.

--- Formula/netcup.rb
+++ Formula/netcup.rb
@@ -1,27 +1,27 @@
 class Netcup < Formula
   desc "CLI and ops TUI for the netcup SCP REST API"
   homepage "https://github.com/brandonkramer/netcup-cli"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.0/netcup_0.1.0_darwin_arm64.tar.gz"
-      sha256 "25cad753d1deba86373a494b2e48e84f54fd9068a08afb82f718508779303cc0"
+      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.1/netcup_0.1.1_darwin_arm64.tar.gz"
+      sha256 "697a68b247fece90ec89902beda39e3e957939dc00be0cdcd9c85c496e940a2c"
     end
     on_intel do
-      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.0/netcup_0.1.0_darwin_amd64.tar.gz"
-      sha256 "e8da8dbd6d5ac49db53d2a4387daec65cab415fb82ab63179ff32d893d3a88bc"
+      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.1/netcup_0.1.1_darwin_amd64.tar.gz"
+      sha256 "a45aa7c20ddea40fc88739b26bf0ce1f38bb8fb5a39701ce6f29f72db54b5758"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.0/netcup_0.1.0_linux_arm64.tar.gz"
-      sha256 "35a6001f55137f8b143657365aa0cce49d3f422a28614b13678920dca2df5750"
+      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.1/netcup_0.1.1_linux_arm64.tar.gz"
+      sha256 "02e11454a7f85e7051c2e54c5f7f76ea47cfe1694af68e19626cf041b00fe54d"
     end
     on_intel do
-      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.0/netcup_0.1.0_linux_amd64.tar.gz"
-      sha256 "34a8fc4ce17471aba3cfe5ad447e634c7f461c66d52621cd433588b655b5108c"
+      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.1/netcup_0.1.1_linux_amd64.tar.gz"
+      sha256 "7296f18aa1e310836c2243cba07de10c35fe707143b51ddc743c6d933d0a2b1e"
     end
   end
 
