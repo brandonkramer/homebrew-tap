6f045d944be803c7cdb96d5870c0c9d409eda15a
brandonkramer <4xsquid@gmail.com>
2026-07-19 04:52:48 +0300

    Bump netcup to 0.2.0.

--- Formula/netcup.rb
+++ Formula/netcup.rb
@@ -1,27 +1,27 @@
 class Netcup < Formula
   desc "CLI and ops TUI for the netcup SCP REST API"
   homepage "https://github.com/brandonkramer/netcup-cli"
-  version "0.1.2"
+  version "0.2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.2/netcup_0.1.2_darwin_arm64.tar.gz"
-      sha256 "5c6caba7a00a09b48e36b4170db0e634cd8a779ff8098c9fa7c35e33cbc87246"
+      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.2.0/netcup_0.2.0_darwin_arm64.tar.gz"
+      sha256 "77c845ac98e815ccbad32c53bd74a37e3254fda761ac80b2e86e0569d6f4c528"
     end
     on_intel do
-      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.2/netcup_0.1.2_darwin_amd64.tar.gz"
-      sha256 "9fa6b1e369487909c540cca917fee5dbcbd723eb3713aabc465cf3d351992d4b"
+      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.2.0/netcup_0.2.0_darwin_amd64.tar.gz"
+      sha256 "1bcb04ad4b79d833e9c1b2a1c813d7fc63b97f9623c8ad95029f1bcb575b2618"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.2/netcup_0.1.2_linux_arm64.tar.gz"
-      sha256 "77c9bf1af1b677d2a8794b3e9699d41283f15babf77fc5618e91b6e53d577388"
+      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.2.0/netcup_0.2.0_linux_arm64.tar.gz"
+      sha256 "ff69aace0c3d9ec83781cffe19b28e444f2ee6f87239c63a864bd1b72c800ec2"
     end
     on_intel do
-      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.1.2/netcup_0.1.2_linux_amd64.tar.gz"
-      sha256 "316ee695eb40e0b8c3ab06bb4275e591f09a5493cba3ae58efd05f05877206be"
+      url "https://github.com/brandonkramer/netcup-cli/releases/download/v0.2.0/netcup_0.2.0_linux_amd64.tar.gz"
+      sha256 "7630f357e684fb15b0f011b248c2b651fb15cb1562ad0084eccf0154d3c8722f"
     end
   end
 
